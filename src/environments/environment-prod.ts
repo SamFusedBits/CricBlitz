0c309f8a631c99f7917d65a1dd6f35e12bbfec7d
Sahil Memon <sahil.infosphere@gmail.com>
2024-06-01 23:31:35 +0530

    Modified environment files

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://cricblitz.vercel.app/', //This will send the request to the backendURL, stored as environment variable 
+    apiUrl: 'https://cricblitz.vercel.app', //This will send the request to the backendURL, stored as environment variable 
     apiKey: '87c5d120ae02459fb660f9188bceb3fc'    //This will get the api key stored as an environment variable
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://cricblitz.vercel.app/',
+    apiUrl: 'https://cricblitz.vercel.app',
     apiKey: '87c5d120ae02459fb660f9188bceb3fc'
 };
