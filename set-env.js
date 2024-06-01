dde874100ee06e671d1b9c617f56f60e2fa666c5
Sahil Memon <sahil.infosphere@gmail.com>
2024-06-01 19:35:16 +0530

    Updated set-env.js to use ES6 import, added more files to replace, and modified console logs and error handling

--- set-env.js
+++ set-env.js
@@ -1,21 +1,20 @@
-const replace = require('replace-in-file');
+import { sync } from 'replace-in-file';
 const apiUrl = process.env.API_URL;
 const apiKey = process.env.API_KEY;
 
 const options = {
-  files: './src/environments/environment.prod.ts',
+  files: ['./src/environments/environment.ts','./src/environments/environment.prod.ts', './src/environments/environment.development.ts', '.\src\app\pages\home\home.component.ts'],
   from: [/apiUrl: 'API_URL'/g, /apiKey: 'API_KEY'/g],
   to: [`apiUrl: '${apiUrl}'`, `apiKey: '${apiKey}'`],
 };
 
 try {
-  let changedFiles = replace.sync(options);
-  if (changedFiles == 0) {
+  let changedFiles = sync(options);
+  console.log(`Changed files: ${changedFiles}`);
+  if (changedFiles.length === 0) {
     throw "Please make sure that file '" + options.files + "' has \"apiUrl: 'API_URL'\" & \"apiKey: 'API_KEY'\"";
   }
-  console.log('Build version set: ' + apiUrl);
-}
-catch (error) {
+} catch (error) {
   console.error('Error occurred:', error);
-  throw error
+  throw error;
 }
