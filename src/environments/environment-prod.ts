2f3288d7e8760a242bfa8949a7ee816c711feaed
Sahil Memon <sahil.infosphere@gmail.com>
2024-06-02 08:34:05 +0530

    Refactor environment files

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://cricblitz.vercel.app', //This will send the request to the backendURL, stored as environment variable 
+    apiUrl: 'https://cricblitz.vercel.app',
 };
